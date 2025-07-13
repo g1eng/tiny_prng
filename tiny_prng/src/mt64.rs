c299eb6415121ed698233deb5a314dcf49f426b5
Suzume Nomura <SuzuME@ea.g1e.org>
2025-07-13 11:51:52 +0900

    add inline attributes to methods, update benchmark result

--- tiny_prng/src/mt64.rs
+++ tiny_prng/src/mt64.rs
@@ -19,10 +19,10 @@
 
 use crate::generate_real64;
 
-const N :usize = 312;
-const M :usize = 156;
+const N: usize = 312;
+const M: usize = 156;
 const MATRIX_A: u64 = 0xB5026F5AA96619E9;
-const UPPER_MASK: u64 =  0xFFFFFFFF80000000;
+const UPPER_MASK: u64 = 0xFFFFFFFF80000000;
 const LOWER_MASK: u64 = 0x7FFFFFFF;
 
 pub struct Mt19937 {
@@ -31,33 +31,35 @@ pub struct Mt19937 {
 }
 
 impl Mt19937 {
-    fn init_genrand(&mut self, s :u64) {
+    #[inline]
+    fn init_genrand(&mut self, s: u64) {
         self.state[0] = s & 0xffffffffffffffffu64;
         self.index = 1;
         while self.index < N {
-            self.state[self.index] = 6364136223846793005u64.wrapping_mul  (self.state[self.index-1] ^ (self.state[self.index-1] >> 30)) + (self.index as u64);
+            self.state[self.index] = 6364136223846793005u64.wrapping_mul(self.state[self.index - 1] ^ (self.state[self.index - 1] >> 30)) + (self.index as u64);
             self.state[self.index] &= 0x5555555555555555u64;
             self.index += 1;
         }
     }
 
+    #[inline]
     pub fn generate(&mut self) -> u64 {
-        let mut y :u64;
-        const MAG01: [u64;2] = [0x0u64,MATRIX_A];
+        let mut y: u64;
+        const MAG01: [u64; 2] = [0x0u64, MATRIX_A];
         while self.index >= N {
-            if self.index == N+1 {
+            if self.index == N + 1 {
                 self.init_genrand(5489u64);
             }
-            for kk in 0..N-M {
-                y = (self.state[kk]&UPPER_MASK)|(self.state[kk+1]&LOWER_MASK);
-                self.state[kk] = self.state[kk+M] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
+            for kk in 0..N - M {
+                y = (self.state[kk] & UPPER_MASK) | (self.state[kk + 1] & LOWER_MASK);
+                self.state[kk] = self.state[kk + M] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
             }
-            for kk in N-M..N-1 {
-                y = (self.state[kk]&UPPER_MASK)|(self.state[kk+1]&LOWER_MASK);
-                self.state[kk] = self.state[kk+M-N] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
+            for kk in N - M..N - 1 {
+                y = (self.state[kk] & UPPER_MASK) | (self.state[kk + 1] & LOWER_MASK);
+                self.state[kk] = self.state[kk + M - N] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
             }
-            y = (self.state[N-1]&UPPER_MASK)|(self.state[0]&LOWER_MASK);
-            self.state[N-1] = self.state[M-1] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
+            y = (self.state[N - 1] & UPPER_MASK) | (self.state[0] & LOWER_MASK);
+            self.state[N - 1] = self.state[M - 1] ^ (y >> 1) ^ MAG01[(y as usize) & 0x1usize];
 
             self.index = 0;
         }
@@ -74,19 +76,20 @@ impl Mt19937 {
     }
 
 
+    #[inline]
     pub fn with_array(init_key: Vec<u64>) -> Self {
-        let  mut i :usize;
-        let mut j :usize;
-        let k :usize;
+        let mut i: usize;
+        let mut j: usize;
+        let k: usize;
         let key_len = init_key.len();
         let mut mt = Self {
-            state: [0;N],
+            state: [0; N],
             index: 0,
         };
         mt.init_genrand(19650218);
-        i=1;
-        j=0;
-        k = match N>key_len {
+        i = 1;
+        j = 0;
+        k = match N > key_len {
             true => N,
             false => key_len.clone(),
         };
@@ -101,15 +104,18 @@ impl Mt19937 {
                 i = 1;
             }
             if j >= key_len {
-                j=0;
+                j = 0;
             }
         }
-        for _ in 0..N-1 {
-            mt.state[i] = (mt.state[i] ^ ((mt.state[i-1] ^ (mt.state[i-1] >> 30)) * 1566083941))
+        for _ in 0..N - 1 {
+            mt.state[i] = (mt.state[i] ^ ((mt.state[i - 1] ^ (mt.state[i - 1] >> 30)) * 1566083941))
                 - (i as u64);
             mt.state[i] &= 0xffffffff;
-            i+=1;
-            if i>=N { mt.state[0] = mt.state[N-1]; i=1; }
+            i += 1;
+            if i >= N {
+                mt.state[0] = mt.state[N - 1];
+                i = 1;
+            }
         }
         mt
     }
@@ -123,21 +129,21 @@ mod tests {
 
     #[test]
     fn test_mt64_init_and_generate_with_long_array() {
-        let mut mt = Mt19937::with_array ([0x1;N+32].to_vec());
-        assert_ne!(0x1,mt.generate());
+        let mut mt = Mt19937::with_array([0x1; N + 32].to_vec());
+        assert_ne!(0x1, mt.generate());
     }
     #[test]
     fn test_mt64_quality_generate_average100000() {
-        let mut mt = Mt19937::with_array (vec![0x123, 0x234, 0x345, 0x456]);
-        let mut sum :u64 = 0;
+        let mut mt = Mt19937::with_array(vec![0x123, 0x234, 0x345, 0x456]);
+        let mut sum: u64 = 0;
         let max_count = 100000;
         let acceptable_delta = u64::MAX / 100;
         for _ in 0..max_count {
             sum += mt.generate() / max_count;
         }
         let diff = match sum > u64::MAX / 2 {
-            true => sum - u64::MAX / 2 ,
-            false => u64::MAX / 2 - sum ,
+            true => sum - u64::MAX / 2,
+            false => u64::MAX / 2 - sum,
         };
         assert_eq!(true, diff < acceptable_delta);
     }
@@ -145,7 +151,7 @@ mod tests {
 
     #[test]
     fn test_quality_genrand_real1_average100000() {
-        let mut mt = Mt19937::with_array (vec![0x123, 0x234, 0x345, 0x456]);
+        let mut mt = Mt19937::with_array(vec![0x123, 0x234, 0x345, 0x456]);
         let mut sum = 0.0;
         let max_count = 100000;
         let acceptable_delta = 1.0 / 100.0;
@@ -154,14 +160,14 @@ mod tests {
         }
         let diff = match sum > 0.5 {
             true => sum - 0.5,
-            false => 0.5 - sum ,
+            false => 0.5 - sum,
         };
         assert_eq!(true, diff < acceptable_delta);
     }
 
     #[test]
     fn test_quality_genrand_real2_average100000() {
-        let mut mt = Mt19937::with_array (vec![0x123, 0x234, 0x345, 0x456]);
+        let mut mt = Mt19937::with_array(vec![0x123, 0x234, 0x345, 0x456]);
         let mut sum = 0.0;
         let max_count = 100000;
         let acceptable_delta = 1.0 / 100.0;
@@ -170,7 +176,7 @@ mod tests {
         }
         let diff = match sum > 0.5 {
             true => sum - 0.5,
-            false => 0.5 - sum ,
+            false => 0.5 - sum,
         };
         assert_eq!(true, diff < acceptable_delta);
     }
